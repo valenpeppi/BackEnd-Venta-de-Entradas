2ee356a5b23d18819347102417b910a85394991b
gianZaba <gianzabaleta6@hotmail.com>
2025-07-17 11:45:10 -0300

    errores tipado

--- src/routes/sales.router.js
+++ src/routes/sales.router.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const SalesController = require('../controllers/sales.controller');
-const { validateSale } = require('../middlewares/validators');  //falta hacerlo
-const { authMiddleware } = require('../middlewares/auth');    //falta hacerlo
 
 // Rutas protegidas por autenticación
-router.post('/', authMiddleware, validateSale, SalesController.createSale);
-router.get('/client/:dniClient', authMiddleware, SalesController.getSalesByClient);
+router.post('/', SalesController.createSale);
+router.get('/client/:dniClient', SalesController.getSalesByClient);
 
 module.exports = router;
