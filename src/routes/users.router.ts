3f5eef67feb9b1891e3c658f2067afe10923e006
Valen <peppinovalentin@gmail.com>
2025-07-23 19:00:49 -0300

    agregacion rol en user (user o admin)
    Co-authored-by: Ratti Martin <MartinUTN@users.noreply.github.com>
    Co-authored-by: Gianlucas Zabaleta <gzabaleta@frro.utn.edu.ar>

--- src/routes/users.router.ts
+++ src/routes/users.router.ts
@@ -1,10 +1,9 @@
 import { Router } from 'express';
 // Asegúrate de que users.controller.ts exporta estas funciones nombradas
-import { getAllUsers, createUser } from '../controllers/users.controller';
+import { getAllUsers, createUser,} from '../controllers/users.controller';
 
 const router: Router = Router();
 
 router.get('/', getAllUsers);
 router.post('/', createUser);
-
 export default router;
