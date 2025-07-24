9eb11d2b809e00632e4d8bca95c1121a06409521
MartinUTN <martinratti10@gmail.com>
2025-07-24 20:58:01 -0300

    registerCompany agrega a bdd

--- src/routes/auth.router.ts
+++ src/routes/auth.router.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { login, loginCompany, register, registerCompany } from '../controllers/auth.controller';
+import { login, loginCompany, register, registerCompany } from '../controllers/auth.controller'; // Importa todas las funciones necesarias
+
 const router: Router = Router();
 
 router.post('/login', login);
 router.post('/register', register);
 router.post('/register-company', registerCompany);
-router.post('/login-company', loginCompany);    
+router.post('/login-company', loginCompany);     
 
 export default router;
