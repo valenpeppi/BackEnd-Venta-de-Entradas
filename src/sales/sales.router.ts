25752f159cadc8a3b45aac7b0b1da9f54a35c7de
Agustin Santinelli <agustinsantinelli@gmail.com>
2025-09-25 16:31:58 -0300

    Avance - No anda

--- src/sales/sales.router.ts
+++ src/sales/sales.router.ts
@@ -3,7 +3,6 @@ import SalesController from './sales.controller';
 
 const router: Router = Router();
 
-// Rutas protegidas por autenticación
 router.post('/confirm', SalesController.confirmSale);
 
 export default router;
