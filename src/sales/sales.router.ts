96724988644c44677a6604f431ec11a81f99af6e
Valen <peppinovalentin@gmail.com>
2025-10-01 15:37:29 -0300

    console log en sales.routes

--- src/sales/sales.router.ts
+++ src/sales/sales.router.ts
@@ -3,6 +3,9 @@ import SalesController from './sales.controller';
 
 const router: Router = Router();
 
-router.post('/confirm', SalesController.confirmSale);
+router.post('/confirm', (req, res, next) => {
+  console.log("📩 /api/sales/confirm recibido:", req.body);
+  next();
+}, SalesController.confirmSale);
 
 export default router;
