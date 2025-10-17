9c8b79c3becd5240ca1a3572d6004a10c21632c7
Agustin Santinelli <agustinsantinelli@gmail.com>
2025-10-16 21:35:58 -0300

    Borrado Users + Correccion Json

--- src/users/users.router.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { getAllUsers, createUser,} from './users.controller';
-
-const router: Router = Router();
-
-router.get('/', getAllUsers);
-router.post('/', createUser);
-export default router;
