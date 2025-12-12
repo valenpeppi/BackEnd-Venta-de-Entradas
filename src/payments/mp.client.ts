17813fbb669f7d62388793ca6074a671f2a40cde
gianZaba <gianzabaleta6@hotmail.com>
2025-12-12 11:34:53 -0300

    borrado definitivo de MP

--- src/payments/mp.client.ts
+++
@@ -1,7 +0,0 @@
-import { MercadoPagoConfig, Preference } from 'mercadopago';
-
-export const mp = new MercadoPagoConfig({
-  accessToken: process.env.MP_ACCESS_TOKEN || '',
-});
-
-export const preferences = new Preference(mp);
